cadb8c9d27de0ee0c2362cf8054787c46f010d3d
Marfall <marinefalling@gmail.com>
2019-11-03 21:35:12 +0300

    multiple pointcut

---
+++ AOPProject/PointCutDeclrations/src/com.marfall.springaop/MainApp.java
@@ -0,0 +1,34 @@
+package com.marfall.springaop;
+
+import com.marfall.springaop.dao.AccountDAO;
+import com.marfall.springaop.dao.MembershipDAO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+
+        // read spring-config java class
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // get the bean from spring container"accountDAO",
+        AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+        //get membership bean from spring container
+        MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+
+        // call the business method
+        Account myAccount = new Account();
+        theAccountDAO.addAccount(myAccount, true);
+        theAccountDAO.doWork();
+
+
+        theMembershipDAO.addAccount();
+        theMembershipDAO.goToSleep();
+
+
+        // close the context
+        context.close();
+    }
+}
